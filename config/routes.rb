f5361a1a97788a7940469f7c0643eec9b948cdf2
Sherri Mitchell <rsamitchell@att.net>
2019-10-25 12:33:01 -0400

    adding updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'breaches#index'
 
   get 'breaches', to: 'breaches#index'
-  get 'breaches/show'
+  get 'breaches/year/:year', to: 'breaches#show_breaches_by_year'
+  get 'breaches/state/:state', to: 'breaches#show_breaches_by_state'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
